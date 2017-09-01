3ba3d0417a4ca31ff8942c84b75dc1591d4998cc
Tom Purucker <purucker.tom@gmail.com>
2017-09-01 17:54:20 +0000

    rvm update for figure 4a

--- src/fig_4_stoten.R
+++ src/fig_4_stoten.R
@@ -1,4 +1,6 @@
 barplot(D, main="Mixed Pesticides", xlab = "Pesticide Treatment", ylab = "# of Significant Spectral Features", ylim = c(0,105), col="indianred", names.arg = c("ATZ", "MA", "PZ", "ATZMA", "ATZPZ", "MAPZ", "ATZMAPZ"), font.lab = 2, cex.names=0.75, cex.axis = 0.75, axis.lty=1)
 D <- c(42, 89, 103, 22, 43, 53, 22)
-barplot(C, main="Herbicides", xlab = "Pesticide Treatment", ylab = "# of Significant Spectral Features", ylim = c(0,70), col="indianred", names.arg = c("ATZ", "ME", "D", "ATZD", "ATZME", "MED", "ATZMED"), font.lab = 2, cex.names=0.75, cex.axis = 0.75, axis.lty=1)
+legend ("topright", legend = "B", bty="n")
+barplot(C, main="Herbicides", xlab = "Pesticide Treatment", ylab = "# of Significant Spectral Features", ylim = c(0,70), col="indianred", names.arg = c("ATZ", "ME", "D", "ATZME", "ATZD", "MED", "ATZMED"), font.lab = 2, cex.names=0.75, cex.axis = 0.75, axis.lty=1)
 C <- c(22, 15, 69, 38, 23, 36, 37)
+legend ("topright", legend = "A", bty="n")
